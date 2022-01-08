8ffbc216c8c66bc2ff9f05ddc85ddb637dd390a3
Rodrigo França <rfrancacosta@gmail.com>
2022-01-08 01:26:16 -0300

    EnableScheduling

--- library-api/src/main/java/br/com/franca/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/br/com/franca/libraryapi/LibraryApiApplication.java
@@ -4,7 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
 
@@ -13,6 +16,10 @@ public class LibraryApiApplication {
 		return new ModelMapper();
 	}
 
+	@Scheduled(cron = "0 10 1 1/1 * ?")
+	public void testeDeScheduled(){
+		System.out.println("AGENDAMENTO DE TAREFAS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
 	}
